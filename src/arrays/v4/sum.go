520fdfdfddcdee58e9ae7c9da9636434b9c8354b
Brendan <brendan.b.liu@gmail.com>
2021-05-04 22:58:53 +0800

    refactor v4

---
+++ src/arrays/v4/sum.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Sum(numbers []int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+		fmt.Println(sums, numbers)
+	}
+	return
+}
