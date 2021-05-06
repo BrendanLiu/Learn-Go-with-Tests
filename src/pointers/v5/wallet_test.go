c75528086e698bb6e12dc89e6ef04db3373bc7e1
Brendan <brendan.b.liu@gmail.com>
2021-05-06 22:40:59 +0800

    display error message v5

---
+++ src/pointers/v5/wallet_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t *testing.T, wallet Wallet, want BitCoin) {
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	assertError := func(t *testing.T, got error, want string) {
+		if got == nil {
+			// Fatal 如果被调用，将不会进行接下来的测试
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(BitCoin(10))
+		assertBalance(t, wallet, BitCoin(10))
+
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: BitCoin(20)}
+		wallet.Withdraw(BitCoin(10))
+		assertBalance(t, wallet, BitCoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{BitCoin(20)}
+		err := wallet.Withdraw(BitCoin(100))
+
+		assertBalance(t, wallet, BitCoin(20))
+		assertError(t, err, "cannot withdraw, insufficient funds")
+	})
+}
---
+++ src/pointers/v5/wallte.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type BitCoin int
+
+type Wallet struct {
+	balance BitCoin
+}
+
+func (w *Wallet) Deposit(amount BitCoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount BitCoin) error {
+
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() BitCoin {
+	return w.balance
+}
+
+// String() 方法让现有BitCoin类型添加一些领域内特定的功能
+func (b BitCoin) String() string {
+	return fmt.Sprintf("%d Btc", b)
+}
