8a794d6e02919143dffb1148c32c05f4dc477243
Brendan <brendan.b.liu@gmail.com>
2021-05-06 21:12:10 +0800

    withdraw v3

--- src/pointers/v3/wallet_test.go
+++ src/pointers/v3/wallet_test.go
@@ -5,14 +5,30 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(BitCoin(10))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	got := wallet.Balance()
-	want := BitCoin(20)
+		wallet.Deposit(BitCoin(10))
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		got := wallet.Balance()
+		want := BitCoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: BitCoin(20)}
+
+		wallet.Withdraw(BitCoin(10))
+
+		got := wallet.Balance()
+		want := BitCoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
--- src/pointers/v3/wallte.go
+++ src/pointers/v3/wallte.go
@@ -12,6 +12,10 @@ func (w *Wallet) Deposit(amount BitCoin) {
 	w.balance += amount
 }
 
+func (w *Wallet) Withdraw(amount BitCoin) {
+	w.balance -= amount
+}
+
 func (w *Wallet) Balance() BitCoin {
 	return w.balance
 }
