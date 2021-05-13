aff9ddc28d1a67004f3c8c79f86c193e8f565d53
Brendan <brendan.b.liu@gmail.com>
2021-05-13 22:14:22 +0800

    reflection v2

---
+++ src/reflection/v2/reflection_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Brendan"},
+			[]string{"Brendan"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Brendan", "China"},
+			[]string{"Brendan", "China"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
