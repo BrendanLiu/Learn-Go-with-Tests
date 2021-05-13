b2d7c210bf051c291e6605c88b6638a93354ded3
Brendan <brendan.b.liu@gmail.com>
2021-05-13 22:22:06 +0800

    reflection v2

--- src/reflection/v2/reflection.go
+++ src/reflection/v2/reflection.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
 )
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 
-	fmt.Println(val)
-
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		fmt.Println(field)
 		fn(field.String())
 	}
 }
